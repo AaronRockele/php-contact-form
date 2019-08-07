e4680f2a520adffd1771f82ea81040c006ba64a3
Aaron Rockele <aaron_rockele@MacBook-Pro-van-Aaron.local>
2019-08-07 11:03:15 +0200

    fixed password issue

--- form_process.php
+++ form_process.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php';
-include 'form_password.php';
+
 $name_error = $email_error = $message_error = "";
 $name = $email = $message = $success = "";
 
@@ -38,36 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
         }
 
         $mail = new PHPMailer(true);
-
-        try {
-          //Server settings
-        $mail->isSMTP();                                            // Set mailer to use SMTP
-        $mail->Host       = 'smtp.gmail.com;';                      // Specify main and backup SMTP servers
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'aa.rockele@gmail.com';                 // SMTP username
-        $mail->Password   = $password;                              // SMTP password
-        $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-        $mail->Port       = 587;  
-                                                                    // TCP port to connect to
-
-          //Recipients
-        $mail->setFrom($email);
-        $mail->addAddress('aa.rockele@gmail.com');                  // Add a recipient
-        $mail->addReplyTo($email);
-
-
-          // Content
-          $mail->isHTML(true);                                      // Set email format to HTML
-        $mail->Subject = 'Contact';
-        $mail->Body    = $message_body;
-        $mail->AltBody = 'Thanks for contacting us. you will recieve an answer in a few days.';
-        
-        $mail->send();
-            $success = 'mission accomplished!';
-            
-    } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
+        include 'form_password.php';
     }
 }
 function test_input($data) {
